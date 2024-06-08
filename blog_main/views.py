f8a8ec81c66f6562a9649d2e0d7220ac684b3555
MASTERPOP-N <duopoc@gmail.com>
2024-06-08 17:19:47 +0700

    tab menu

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render, redirect
-
+from blogs.models import Category, Blog
 def home(request):
-    return render(request, 'index.html')
+    categories = Category.objects.all()
+    featured_post = Blog.objects.filter(is_featured=True)
+
+    context = {
+        'categories': categories,
+        'featured_post': featured_post,
+        }
+
+    return render(request, 'index.html', context)
